91c39c9a9a8c6e7d6e96821405da4b757c49c325
Xin Wang <xin@Xins-MacBook-Air.local>
2019-10-08 20:47:24 -0400

    Changes to be committed: 	new file:   unsigned.cc

---
+++ unsigned/unsigned.cc
@@ -0,0 +1,49 @@
+/*
+ * This is to show the trick of doing "unsigned int" comparison taking
+ * wrap into considerations.
+ * The trick is to cast to (int), do arithmetic calculation, then compare with 0.
+ */
+#include <stdio.h>
+
+int main ()
+{
+	unsigned int v1 = 0;
+	unsigned int v2 = 0xFFFFFFFF;
+	unsigned int v3 = 0x7FFFFFFF;
+	unsigned int v4 = 0x80000000;
+
+	printf("\n\n\nv1 = 0\n");
+	printf("v1 as unsigned =%u, as signed =%d\n", v1, v1);
+	printf("v2 = 0xFFFFFFFF\n");
+	printf("v2 as unsigned =%u, as signed =%d\n", v2, v2);
+	
+	(v1 < v2)? printf("as unsigned, v1 is less than v2\n"):
+        ((v1 > v2)? printf("as unsigned, v1 is more than v2\n"):           
+		    printf("as unsigned, v1 == v2\n"));
+
+	(((int)v1 - (int)v2) < 0)? printf("as signed consider wrapping, v1 is less than v2\n"):
+        ((((int)v1 - (int)v2) > 0)? printf("as signed consider wrapping, v1 is more than v2\n"):           
+		    printf("as signed, v1 == v2\n"));
+
+	(((long int)v1 - (long int)v2) < 0)? printf("as long signed, v1 is less than v2\n\n"):
+        ((((long int)v1 - (long int)v2) > 0)? printf("as long signed, v1 is more than v2\n\n"):           
+		    printf("as long signed, v1 == v2\n\n"));
+
+	printf("\n\n\nv3 = 0x7FFFFFFF\n");
+	printf("v3 as unsigned =%u, as signed =%d\n", v3, v3);
+	printf("v4 = 0x80000000\n");
+	printf("v4 as unsigned =%u, as signed =%d\n", v4, v4);
+
+	(v3 < v4)? printf("as unsigned, v3 is less than v4\n"):
+        ((v3 > v4)? printf("as unsigned, v3 is more than v4\n"):           
+		    printf("as unsigned, v3 == v4\n"));
+
+	(((int)v3 - (int)v4) < 0)? printf("as signed consider wrapping, v3 is still less than v4\n"):
+        ((((int)v3 - (int)v4) > 0)? printf("as signed consider wrapping, v3 is more than v4\n"):           
+		    printf("as signed, v3 == v4\n"));
+
+	(((long int)v3 - (long int)v4) < 0)? printf("as long signed, v3 is still less than v4\n\n"):
+        ((((long int)v3 - (long int)v4) > 0)? printf("as long signed, v3 is more than v4\n\n"):           
+		    printf("as long signed, v3 == v4\n\n"));
+	return 0;
+}

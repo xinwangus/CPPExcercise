0e085c683fa38aff82dfc4bf0e41c748d655abe6
Xin Wang <xin@Xins-MacBook-Air.local>
2021-01-16 17:48:04 -0500

    Changes to be committed: 	modified:   space_into_word.cc

--- Kevin_Problem/space_into_word.cc
+++ Kevin_Problem/space_into_word.cc
@@ -7,13 +7,21 @@
 
 #include <string>
 #include <iostream>
+#include <unordered_set>
 
 using namespace std;
 
 bool lookup(const string &in)
 {
-	// todo use dictionary API
-	return true;
+	unordered_set<string> dict;
+	dict.insert("Hello");	
+	dict.insert("World");	
+
+	if (dict.find(in) != dict.end()) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 // recursive
@@ -22,22 +30,22 @@ bool break_up(const string& in, string &out)
 {
 	if (in.size() == 0) return false;
 
+	// Greedy: always check the whole string first
+	if (lookup(in)) {
+		out = in;
+		return true;
+	}
+
 	// must have stop point!
 	if (in.size() == 1) {
-		if (lookup(in)) {
-			out = in;
-			return true;
-		} else {
-			return false;
-		}
+		return false;
         }	
 
-	string out_r;
 	for (int i = 0; i < in.size(); i++) {
 		// the 2nd is length, not end position!
 		if (lookup(in.substr(0, i+1)) &&
-		    break_up(in.substr(i+1, in.size()-1-i), out_r)) {
-			out = in.substr(0, i+1) + " " + out_r;
+		    break_up(in.substr(i+1, in.size()-1-i), out)) {
+			out.insert(0, in.substr(0, i+1) + " ");
 			return true;
 		}
 	}
